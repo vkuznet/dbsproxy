97a3888657528a261f0ffdd25172c528ba974567
Valentin Kuznetsov <vkuznet@gmail.com>
2021-08-03 18:11:13 -0400

    Add time printouts for send and collect

--- handlers.go
+++ handlers.go
@@ -22,6 +22,8 @@ func getServer() string {
 // helper function to send request to backend server
 // func send(rurl string, w http.ResponseWriter, r *http.Request, wg *sync.WaitGroup) error {
 func send(rurl string, ch chan []byte, r *http.Request, wg *sync.WaitGroup) error {
+	start := time.Now()
+	defer log.Println("send %s %v", r, time.Since(start))
 	defer wg.Done()
 	// send HTTP request to backend server
 	client := http.Client{}
@@ -55,6 +57,8 @@ func send(rurl string, ch chan []byte, r *http.Request, wg *sync.WaitGroup) erro
 
 // helper function to collect results from goroutines and write them to reponse writer
 func collect(w http.ResponseWriter, ch chan []byte, terminate chan bool) {
+	start := time.Now()
+	defer log.Println("collect %v", time.Since(start))
 	w.Write([]byte("[\n"))
 	defer w.Write([]byte("]\n"))
 	var sep bool
